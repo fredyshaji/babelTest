16ef9540263bdc80c167c40b57474e55e937f079
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2022-08-15 15:01:18 +0200

    feat: Initisl setup

---
+++ BabbelTest/BabbelTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BabbelTest
+//
+//  Created by Fredy Shaji on 2022-08-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
