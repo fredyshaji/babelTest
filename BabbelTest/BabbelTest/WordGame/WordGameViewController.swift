46b9218ef5b05e3a98ec9b15175bb3226df6a87e
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2022-08-15 18:38:18 +0200

    feat: Load questions using a simple logic

--- BabbelTest/BabbelTest/WordGame/WordGameViewController.swift
+++ BabbelTest/BabbelTest/WordGame/WordGameViewController.swift
@@ -18,24 +18,53 @@ class WordGameViewController: UIViewController {
 
     var viewModel = WordGameViewModel()
     
-    let correctAttemptsTitle = "Correct attempts: "
-    let wrongAttemptsTitle = "Wrong attempts: "
-    let correctTitle = "Correct"
-    let wrongTitle = "Wrong"
+    private let correctAttemptsTitle = "Correct attempts: "
+    private let wrongAttemptsTitle = "Wrong attempts: "
+    private let correctTitle = "Correct"
+    private let wrongTitle = "Wrong"
+    private var correctCount = 0
+    private var incorrectCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setUpUI()
         viewModel.fetchData()
+        loadQuestion()
     }
 
     func setUpUI() {
-        correctAttemptsCountLabel.text = correctAttemptsTitle + "0"
-        wrongAttemptsCountLabel.text = wrongAttemptsTitle + "0"
+        updateScore()
         correctChoiceButton.titleLabel?.text = correctTitle
         incorrectChoiceButton.titleLabel?.text = wrongTitle
     }
 
+    func loadQuestion() {
+        let question = viewModel.loadQuestion()
+        self.spanishWordLabel.text = question.spanishText
+        self.englishWordLabel.text = question.englishText
+    }
+
+    func updateScore() {
+        correctAttemptsCountLabel.text = correctAttemptsTitle + String(correctCount)
+        wrongAttemptsCountLabel.text = wrongAttemptsTitle + String(incorrectCount)
+        loadQuestion()
+    }
+
+    // MARK :- Button Ac tions
+    
+    @IBAction func correctButtonClicked(sender: UIButton) {
+        print("button Clicked")
+        let answer = viewModel.checkAnswer()
+        answer ? (correctCount += 1) : (incorrectCount += 1)
+        updateScore()
+    }
+
+    @IBAction func incorrectButtonClicked(sender: UIButton) {
+        print("button Clicked")
+        let answer = viewModel.checkAnswer()
+        answer ? (incorrectCount += 1) : (correctCount += 1)
+        updateScore()
+    }
 }
 
--- BabbelTest/BabbelTest/WordGame/WordGameViewModel.swift
+++ BabbelTest/BabbelTest/WordGame/WordGameViewModel.swift
@@ -10,6 +10,11 @@ import Foundation
 final class WordGameViewModel {
     
     var wordsArray = [WordGame]()
+    var questionCount = 0.0
+    var correctPairCount = 0.0
+    var truePercent = 0.0
+    var currentQuestion = WordGame()
+    var wordsDict = [String:String]()
     
     func fetchData() {
         if let path = Bundle.main.path(forResource: "words", ofType: "json") {
@@ -17,9 +22,43 @@ final class WordGameViewModel {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
                 let jsonResult = try JSONDecoder().decode([WordGame].self, from: data)
                 wordsArray = jsonResult
+                wordsDict = wordsArray.reduce([String:String]()) { (result, word) -> [String:String] in
+                    var result = result
+                    result[word.englishText] = word.spanishText
+                    return result
+                }
             } catch {
                 print("Failed to decode json")
             }
         }
+    }
+
+    func loadQuestion() -> WordGame {
+        currentQuestion = WordGame()
+        if truePercent > 25 {
+            currentQuestion.englishText = wordsArray.randomElement()?.englishText ?? ""
+            currentQuestion.spanishText = wordsArray.randomElement()?.spanishText ?? ""
+            return currentQuestion
+        } else {
+            let correctWord = wordsArray.randomElement()
+            currentQuestion.englishText = correctWord?.englishText ?? ""
+            currentQuestion.spanishText = correctWord?.spanishText ?? ""
+            return currentQuestion
+        }
+    }
+
+    func checkAnswer() -> Bool {
+        let correctTranslation = wordsDict[currentQuestion.englishText] == currentQuestion.spanishText ? true : false
+        if correctTranslation {
+            correctPairCount += 1
+        }
+        questionCount += 1
+        updateTruePercent()
+        return correctTranslation
+    }
+
+    func updateTruePercent() {
+        truePercent = (correctPairCount/questionCount) * 100
+        print("********\(truePercent)********")
     }
 }
