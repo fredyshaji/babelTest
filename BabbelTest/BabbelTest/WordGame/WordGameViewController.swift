8449905014f5a953cf332f87cca158122aefd6d4
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2022-08-16 17:49:33 +0200

    feat: Add timer

--- BabbelTest/BabbelTest/WordGame/WordGameViewController.swift
+++ BabbelTest/BabbelTest/WordGame/WordGameViewController.swift
@@ -17,6 +17,7 @@ class WordGameViewController: UIViewController {
     @IBOutlet weak var incorrectChoiceButton: UIButton!
 
     var viewModel = WordGameViewModel()
+    var timer = Timer()
     
     private let correctAttemptsTitle = "Correct attempts: "
     private let wrongAttemptsTitle = "Wrong attempts: "
@@ -45,18 +46,28 @@ class WordGameViewController: UIViewController {
     }
 
     func loadQuestion() {
+        timer.invalidate()
         let question = viewModel.loadQuestion()
         self.spanishWordLabel.text = question.spanishText
         self.englishWordLabel.text = question.englishText
+        startTimer()
     }
 
     func updateScore() {
         correctAttemptsCountLabel.text = correctAttemptsTitle + String(correctCount)
         wrongAttemptsCountLabel.text = wrongAttemptsTitle + String(incorrectCount)
-        loadQuestion()
+        if viewModel.questionCount >= 15 || incorrectCount >= 3 {
+            exit(0)
+        } else {
+            loadQuestion()
+        }
     }
 
-    // MARK :- Button Ac tions
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    }
+
+    // MARK :- Actions
     
     @IBAction func correctButtonClicked(sender: UIButton) {
         print("button Clicked")
@@ -69,6 +80,11 @@ class WordGameViewController: UIViewController {
         print("button Clicked")
         let answer = viewModel.checkAnswer()
         answer ? (incorrectCount += 1) : (correctCount += 1)
+        updateScore()
+    }
+
+    @objc func timerAction() {
+        incorrectCount += 1
         updateScore()
     }
 }
